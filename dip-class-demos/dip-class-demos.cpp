aa55053bc9f973618948c6e6c3aff805e0aae50e
Zhu Li <crazyjoe874@msn.com>
2020-04-17 00:37:22 +0800

    main函数添加计时

--- dip-class-demos/dip-class-demos.cpp
+++ dip-class-demos/dip-class-demos.cpp
@@ -5,13 +5,21 @@
 #include <opencv.hpp>
 
 using namespace cv;
+using namespace std;
 
 int main()
 {
+	//开始计时
+	double start = static_cast<double>(cvGetTickCount());
 
 
+	//结束计时
+	double time = ((double)cvGetTickCount() - start) / cvGetTickFrequency();
+	//显示时间
+	cout << "processing time:" << time / 1000 << "ms" << endl;
 
-	waitKey(0);
+	//等待键盘响应，按任意键结束程序
+	system("pause");
     return 0;
 }
 
