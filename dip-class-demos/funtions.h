d0171234a5cb8de93006d2867282d73cfa6547b8
Zhu Li <crazyjoe874@msn.com>
2020-04-19 11:32:51 +0800

    注释添加

--- dip-class-demos/funtions.h
+++ dip-class-demos/funtions.h
@@ -1,10 +1,12 @@
 #pragma once
 
+
+
+//课前编程热身练习的示例程序，2种质数筛选算法
 //挑选质数
 //试除法
 void pickPrime(int maxVal, bool ifPrint);
 bool isPrime(int x);
-
 //挑选质数
 //埃拉托斯特尼筛法(sieve of Eratosthenes)
 void EratosSieve(int maxVal,bool ifPrint);
