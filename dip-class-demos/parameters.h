41001cbd4dd62bc6c283aa34baf0e763b9091b8f
Zhu Li <crazyjoe874@msn.com>
2020-06-05 10:38:19 +0800

    增加两个常用参数

--- dip-class-demos/parameters.h
+++ dip-class-demos/parameters.h
@@ -1,4 +1,9 @@
 #pragma once
 
 
-//全局参数文件
+//选择需要演示的demo
+#define DEMO_METHOD		0	
+
+//参数设置
+#define USE_CAMERA		false							//ture: 使用摄像头作为输入	false：读取本地视频
+#define VIDEO_PATH		"../testImages\\vtest.avi"		//如果读取本地视频，则使用该路径
