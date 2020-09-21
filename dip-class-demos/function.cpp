16e4c4b97f3beb7835a3e8a3515338fa2a067d04
lin ge <397367585@qq.com>
2020-09-21 15:44:21 +0800

    1

--- dip-class-demos/function.cpp
+++ dip-class-demos/function.cpp
@@ -184,7 +184,6 @@ void Convolution_app()
 		//对detecMat进行初始化
 		frame.copyTo(maskMat);
 
-
 		//利用inRange函数对图片进行hsv筛选（选出人脸部分）
 		cv::inRange(hsvMat, Scalar(i_minH, i_minS, i_minV), Scalar(i_maxH, i_maxS, i_maxV), maskMat);
 
